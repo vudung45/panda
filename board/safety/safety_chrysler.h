d2b7ab7eddf2a98e35ef926a4d729443e83b730a
Jason Wen <haibin.wen3@gmail.com>
2022-08-28 13:06:44 -0400

    fixup! sunnypilot: MADS - FCA

--- board/safety/safety_chrysler.h
+++ board/safety/safety_chrysler.h
@@ -275,7 +275,7 @@ static int chrysler_tx_hook(CANPacket_t *to_send, bool longitudinal_allowed) {
   if (addr == chrysler_addrs->CRUISE_BUTTONS) {
     const bool is_cancel = GET_BYTE(to_send, 0) == 1U;
     const bool is_resume = GET_BYTE(to_send, 0) == 0x10U;
-    const bool allowed = is_cancel || (is_resume && controls_allowed);
+    const bool allowed = is_cancel || (is_resume && controls_allowed && controls_allowed_long);
     if (!allowed) {
       tx = 0;
     }
