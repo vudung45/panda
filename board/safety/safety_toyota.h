412a385ee1f0ef26510210bf3a7787cfe5d67f07
Jason Wen <haibin.wen3@gmail.com>
2024-07-01 03:11:59 +0000

    Revert "Toyota: Enhance BSM support"

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -45,7 +45,7 @@ const int TOYOTA_GAS_INTERCEPTOR_THRSLD = 805;
 
 // Stock longitudinal
 #define TOYOTA_COMMON_TX_MSGS                                                                                     \
-  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8},  {0x750, 0, 8}, /* sp - white list 0x750 for Enhanced Diagnostic Request */ /* LKAS + LTA + ACC & PCM cancel cmds */  \
+  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8},  /* LKAS + LTA + ACC & PCM cancel cmds */  \
 
 #define TOYOTA_COMMON_LONG_TX_MSGS                                                                                                          \
   TOYOTA_COMMON_TX_MSGS                                                                                                                     \
@@ -373,22 +373,8 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
   if (addr == 0x750) {
     // this address is sub-addressed. only allow tester present to radar (0xF)
     bool invalid_uds_msg = (GET_BYTES(to_send, 0, 4) != 0x003E020FU) || (GET_BYTES(to_send, 4, 4) != 0x0U);
-
-    // SP: Secret sauce from dp. (ask @rav4kumar prior to modifing)
-    // Enhanced BSM
-    bool sp_invalid_uds_msgs = (GET_BYTES(to_send, 0, 4) != 0x10002141) ||
-                               (GET_BYTES(to_send, 0, 4) != 0x60100241) ||
-                               (GET_BYTES(to_send, 0, 4) != 0x69210241);
-
-    sp_invalid_uds_msgs |= (GET_BYTES(to_send, 0, 4) != 0x10002142) ||
-                           (GET_BYTES(to_send, 0, 4) != 0x60100242) ||
-                           (GET_BYTES(to_send, 0, 4) != 0x10002142) ||
-                           (GET_BYTES(to_send, 0, 4) != 0x69210242);
-
-    sp_invalid_uds_msgs |= (GET_BYTES(to_send, 0, 4) != 0x11300540);
-
-    if (invalid_uds_msg || sp_invalid_uds_msgs) {
-      tx = false;
+    if (invalid_uds_msg) {
+      tx = 0;
     }
   }
 
