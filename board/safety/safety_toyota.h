5d92bdc6dee1f1bba5062f551bf6938f40723d81
Jaosn Wen <haibin.wen3@gmail.com>
2024-07-29 06:23:50 -0400

    Revert "Toyota: Auto Brake Hold"
    This reverts commit 2b1869bb1b2d2cc3c38ce0ac297dd114649a5e01.

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -367,13 +367,6 @@ static bool toyota_tx_hook(const CANPacket_t *to_send) {
         }
       }
     }
-
-    // SP: auto brake hold https://github.com/AlexandreSato
-    if ((addr == 0x344) && (alternative_experience & ALT_EXP_ALLOW_AEB)) {
-      if (vehicle_moving || gas_pressed || !acc_main_on) {
-        tx = false;
-      }
-    }
   }
 
   // UDS: Only tester present ("\x0F\x02\x3E\x00\x00\x00\x00\x00") allowed on diagnostics address
@@ -450,10 +443,7 @@ static int toyota_fwd_hook(int bus_num, int addr) {
     bool is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
     // in TSS2 the camera does ACC as well, so filter 0x343
     bool is_acc_msg = (addr == 0x343);
-    // SP: block AEB when auto brake hold is active, unblock AEB when auto brake hold is not active
-    bool is_aeb_msg = (addr == 0x344);
-    bool block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal) ||
-                     (is_aeb_msg && (alternative_experience & ALT_EXP_ALLOW_AEB) && !vehicle_moving && !gas_pressed && acc_main_on);
+    bool block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal);
     if (!block_msg) {
       bus_fwd = 0;
     }
